6815707399335b2b0d3d9ae79d8689f685fc11ce
Rax13 <984028161@qq.com>
2024-08-28 20:51:05 +0800

    Init

--- src/Init.java
+++ src/Init.java
@@ -1,2 +1,43 @@
+import it.unisa.dia.gas.jpbc.Element;
+import it.unisa.dia.gas.jpbc.Field;
+import it.unisa.dia.gas.jpbc.Pairing;
+import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
+
 public class Init {
+    public static Pairing pairing = PairingFactory.getPairing("a.properties");
+    public static Field G1, G2, Zr;
+    public static Element g, q;
+    public static int blockNum = 2000;
+    public static int chalNum = 100;
+
+    public static void init() {
+        G1 = pairing.getG1();        //群
+        G2 = pairing.getGT();        //群
+        Zr = pairing.getZr();       //域
+
+        g = Init.G1.newRandomElement().getImmutable();  //生成随机元素
+        q = Init.G1.newRandomElement().getImmutable();
+    }
+    public static Element H1(String element1,Element element2){
+        String hashInputString = element1 + element2.toString();
+
+        //这里必须加getImmutable(),否则后面其数值会变化
+        return Init.pairing.getZr().newElement()
+                .setFromHash(hashInputString.getBytes(), 0, hashInputString.length()).getImmutable();
+    }
+
+    //H2返回值为整数
+    public static Element H2(String element1, Element element2,Element element3){
+        String hashInputString = element1 + element2.toString() + element3.toString();
+
+        //这里必须加getImmutable(),否则后面其数值会变化
+        return Init.pairing.getZr().newElement()
+                .setFromHash(hashInputString.getBytes(), 0, hashInputString.length()).getImmutable();
+    }
+
+    //h返回值为G1类型元素
+    public static Element h(String element1){
+        return Init.pairing.getG1().newElement()
+                .setFromHash(element1.getBytes(), 0, element1.length()).getImmutable();
+    }//h3
 }
