76d3d0181c5dbdd9bb1e4a4281aefd8709a859b7
Rax13 <984028161@qq.com>
2024-09-08 15:53:31 +0800

    Init

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import it.unisa.dia.gas.jpbc.Element;
 public class Main {
     public static void main(String[] args) {
 
-        Element Ai;
         Init.init();
         KGC.init_ks();
         String User_id = "1";
         //Genkey
         User.getPKG(User_id);
-        Element[] result = new Element[3];
+        Element[] result;
         result=KGC.getPKG(User_id);
         if(!User.verifyPKGData(result)){
             System.out.println("User verify PKG data failed");
